e12bdc43814550abf26c440af9d38648dd86f15a
Megan Coyle <mcoyle@usgbc.org>
2017-05-18 10:27:28 -0400

    Add rotator

--- script.js
+++ script.js
@@ -2,4 +2,13 @@ $.getJSON('data.json', function(data) {
   var template = $('#speakerstpl').html();
   var html = Mustache.to_html(template, data);
   $('#carousel').html(html);
+
+  $('#carousel').cycle({
+    fx: 'fade',
+    pause: 1,
+    next: '#next_btn',
+    prev: '#prev_btn',
+    speed: 500,
+    timeout: 10000
+  });
 }); //getJSON
